4566c9b557f62c281a1bd4a250b8a0c2d428552d
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-04 12:18:20 +0300

    loops with for and while

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(strNamesList, eventName){
+    let newList = []
+    
+    for (let count = 0; count < strNamesList.length; count++){
+        newList.push(`Thank you, ${strNamesList[count]}, for the wonderful surprise gift!`)
+
+    }
+    return newList;
+
+}
+function countDown(int){
+
+    while(int >=0){
+        console.log(int)
+        int--
+    }
+
+}
